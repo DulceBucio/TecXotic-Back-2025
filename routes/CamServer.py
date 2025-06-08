b46be05d6a6f763dd85d59236e9888113801947c
Carolina Figueroa <andreacarolina.f28@gmail.com>
2025-06-08 13:46:45 -0600

    routes to post alpha and beta

--- routes/CamServer.py
+++ routes/CamServer.py
@@ -2,6 +2,7 @@ import cv2
 from flask import Response, Blueprint
 from flask_cors import CORS
 from Capture import Capture
+from flask import request
 
 camServer = Blueprint('camServer', __name__)
 CORS(camServer)
@@ -9,11 +10,38 @@ CORS(camServer)
 cap1 = Capture()
 # cap2 = Capture(1)
 
+alphaPos = 60
+betaPos = 60
+
+@camServer.route('/alpha', methods=['POST'])
+def setAlpha():
+    global alphaPos
+    data = request.get_json()
+    if 'alpha' in data:
+        alphaPos = int(data['alpha'])
+        return {'status': 'success', 'alpha': alphaPos}
+    else:
+        return {'status': 'error', 'message': 'Alpha value not provided'}, 400
+
+@camServer.route('/beta', methods=['POST'])
+def setBeta():
+    global betaPos
+    data = request.get_json()
+    if 'beta' in data:
+        betaPos = int(data['beta'])
+        return {'status': 'success', 'beta': betaPos}
+    else:
+        return {'status': 'error', 'message': 'Beta value not provided'}, 400
+
+def brightnessAjustment(img):
+    imgBrightness = cv2.convertScaleAbs(img, alpha=(alphaPos/100), beta=betaPos)
+    return imgBrightness   
 
 def generate(capture):
     while True:
         ret, frame = capture.get_frame()
         if ret:
+            frame = brightnessAjustment(frame)
             (flag, encodedImage) = cv2.imencode(".jpg", frame)
             if not flag:
                 continue
